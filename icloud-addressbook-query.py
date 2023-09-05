47e3cbb3e015433c1f94ea8c45ac6e58db2c8b61
Andrew Ho-Lee <andrewho@andrewho.co.uk>
2023-09-06 00:39:02 +0100

    Update to use python3

--- icloud-addressbook-query.py
+++ icloud-addressbook-query.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import os
@@ -9,42 +9,58 @@ def query_db(keyword):
     """Query the address book for keyword and return the results as a list of
     tuples.
     """
-    sources = os.listdir(os.path.expanduser("~/Library/Application Support/AddressBook/Sources"))
+    sources = os.listdir(
+        os.path.expanduser("~/Library/Application Support/AddressBook/Sources")
+    )
 
     rows = []
 
     for source in sources:
-        newrows = query_source_db(keyword, os.path.expanduser(os.path.join("~/Library/Application Support",
-            "AddressBook/Sources", source, "AddressBook-v22.abcddb")))
+        newrows = query_source_db(
+            keyword,
+            os.path.expanduser(
+                os.path.join(
+                    "~/Library/Application Support",
+                    "AddressBook/Sources",
+                    source,
+                    "AddressBook-v22.abcddb",
+                )
+            ),
+        )
 
         for row in newrows:
             rows.append(row)
 
     return rows
 
+
 def query_source_db(keyword, database):
     connection = sqlite3.connect(database)
     c = connection.cursor()
-    k = ("%%%s%%" % keyword.decode("utf-8"),) * 5
-    c.execute("""SELECT ZABCDEMAILADDRESS.ZADDRESS, ZABCDRECORD.ZTITLE,
+    k = ("%%%s%%" % keyword) * 5
+    k = {"keyword": "%{}%".format(keyword)}
+    c.execute(
+        """SELECT ZABCDEMAILADDRESS.ZADDRESS, ZABCDRECORD.ZTITLE,
               ZABCDRECORD.ZFIRSTNAME, ZABCDRECORD.ZMIDDLENAME,
               ZABCDRECORD.ZLASTNAME, ZABCDRECORD.ZSUFFIX,
               ZABCDRECORD.ZORGANIZATION, ZABCDRECORD.ZDISPLAYFLAGS
               FROM ZABCDEMAILADDRESS
               LEFT OUTER JOIN ZABCDRECORD
               ON ZABCDEMAILADDRESS.ZOWNER = ZABCDRECORD.Z_PK
-              WHERE ZABCDEMAILADDRESS.ZADDRESS LIKE ?
-              OR ZABCDRECORD.ZFIRSTNAME LIKE ?
-              OR ZABCDRECORD.ZMIDDLENAME LIKE ?
-              OR ZABCDRECORD.ZLASTNAME LIKE ?
-              OR ZABCDRECORD.ZORGANIZATION LIKE ?""", k)
+              WHERE ZABCDEMAILADDRESS.ZADDRESS LIKE :keyword
+              OR ZABCDRECORD.ZFIRSTNAME LIKE :keyword
+              OR ZABCDRECORD.ZMIDDLENAME LIKE :keyword
+              OR ZABCDRECORD.ZLASTNAME LIKE :keyword
+              OR ZABCDRECORD.ZORGANIZATION LIKE :keyword""",
+        k,
+    )
     return [row for row in c]
 
 
 def output(results):
     """Take the list of results and print it as per mutt's expected output"""
     if results:
-        print " ".join([str(len(results)), "matching addresses..."])
+        print(" ".join([str(len(results)), "matching addresses..."]))
         for row in results:
             # If the Company checkbox has been ticked (ZDISPLAYFLAGS=1,129) then
             # show the Company name, otherwise use First Last [(Company)]
@@ -60,16 +76,17 @@ def output(results):
                 fn = row[0]
 
             out = "\t".join([row[0], fn])
-            print out.encode("utf-8")
+            print(out)
         sys.exit(0)
     else:
-        print "No matches"
+        print("No matches")
         sys.exit(-1)
 
 
 def main():
     if len(sys.argv) > 1:
         output(query_db(sys.argv[1]))
+
 
 if __name__ == "__main__":
     main()
